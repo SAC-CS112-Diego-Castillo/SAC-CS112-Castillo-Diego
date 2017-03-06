298384906d754864baec94525b58105dd35c1052
SAC-CS112-Diego-Castillo <diego-castillo@nova-student.org>
2017-03-05 19:57:13 -0800

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+
+
+public class addition{
+
+
+public static void main(String[]args)
+{
+// Create a Scanner object
+Scanner input " new Scanner(System.in);
+
+
+// Prompt the user to enter two numbers
+System.out.print("Enter two numbers: ");
+double number1 = input.nextDouble();
+double number2 = input.nextDouble();
+
+
+// Compute sum
+double sum = (number1 + number2);
+
+
+// Display result
+
+
+Promt the user to enter two numbers
+System.out.print("Enter two numbers; ");
+Double number1 = input.nextDouble();
+Double number2 = input.nextDouble();
+
+
+// Compute sum
+Double sum = (number1 + number2);
+
+
+// Display result
+System.out.println("The sum of " + number1 + " " + number2 + " " + " is "
+" + sum);
+
+
+// Compute difference
+double difference = (number1 - number2);
+
+
+// Display result
+System.out.println("The difference of " + number1 + " " + number2 +
+" is " + difference);
+
+
+
+//  Dispute difference
+Double difference = (number1 - number2);
+
+
+// Display result
+System.out.println("The difference of " + number1 + " " + number2 + " is "
+" + difference);
+
+
+// Compute product
+double product = (number1 * number2);
+
+
+// Display result
+System.out.println("The product of " + number1 + " " + number2 + " is "
+" + product);
+
+
+// Compute quoient
+Double quoient = (number1 / number2);
+
+
+// Display result
+System.out.println("The quoient of " + number1 + " " + number2 + " is "
+" + quoient);
+
+
+// Compute quoient
+Double quoient " (number1 / number2);
+
+
+// Display result
+System.out.println("The difference of " + number1 + " " + number2 + " is "
+" + difference);
+
+
+// Compute product
+double product = (number1 * number2);
+
+
+// Display result
+System.out.println("The product of " + number1 + " " + number2 + " is "
+" + product);
+
+
+// Compute quoient
+Double quoient = (number1 / number2);
+
+
+// Display result
+System.out.println("The quoient of " + number1 + " " + number2 + " is "
+" + quoient);
+
+
+// Compute quoient
+Double quoient " (number1 / number2);
+
+
+// Display result
+System.out.println("The product of " + number1 + " " + number2 + " is "
+quoient);
+
+
+// Compute remainder
+Double remainder = (number1 % number2);
+
+
+// Display remainder
+System.out.println("The remainder of " + number + " " + number2 + " is "
+remainder);
+
+
+}
